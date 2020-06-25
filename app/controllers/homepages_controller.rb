d3603da37fc26c31801801faa73a282fc448b093
Powai Leung <pwleung1489@gmail.com>
2020-06-25 05:43:10 +0000

    Input cali about us page

---
+++ app/controllers/homepages_controller.rb
@@ -0,0 +1,34 @@
+class HomepagesController < ApplicationController
+  def index
+  end
+
+  def maintenances
+  end
+
+  def repairs
+  end
+
+  def about_us
+  end
+
+  def deal
+  end
+
+  def deals
+  end
+
+  def contact_us
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def careers
+  end
+
+  def deals_form
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'homepages#index'
+  get 'maintenances', to:'homepages#maintenances'
+  get 'repairs', to:'homepages#repairs'
+  get 'about_us', to:'homepages#about_us'
+  get 'deal', to: 'homepages#deal'
+  get 'deals', to:'homepages#deals'
+  get 'contact_us', to:'homepages#contact_us'
+  get 'terms', to:'homepages#terms'
+  get 'privacy', to:'homepages#privacy'
+  get 'careers', to:'homepages#careers'
+  get 'deals_form', to:'homepages#deals_form'
+  
+
 end
+
+
+
+
+
+
+
+
+
