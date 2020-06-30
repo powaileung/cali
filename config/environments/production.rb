ad52c3b9e8ea5250adb9ea8041e69eb0caca7931
Powai Leung <pwleung1489@gmail.com>
2020-06-30 06:04:45 +0000

    Input Devise App

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com', 
     port: '587',
