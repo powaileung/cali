f0f6a435fe21804ef337c7a3014a24f6f3356692
Powai Leung <pwleung1489@gmail.com>
2020-06-30 07:00:01 +0000

    Input Devise App

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,14 +1,16 @@
 Rails.application.configure do
   
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net', 
+    address: 'smtp.gmail.com', 
     port: '587',
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
     enable_starttls_auto: true
-  }
   
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will take precedence over those in config/application.rb.
