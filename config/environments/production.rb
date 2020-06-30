dfb318141c1e644bd219a36e27d1666469525d12
Powai Leung <pwleung1489@gmail.com>
2020-06-30 08:34:06 +0000

    Input Devise App

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -8,7 +7,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com', 
     port: '587',
     domain: 'gmail.com',
-    user_name: ENV['GMAIL_ADDRESS'],
+    user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
