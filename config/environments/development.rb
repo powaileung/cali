8ae0d43404079712f3c80b5223a1c924dfa40625
Powai Leung <pwleung1489@gmail.com>
2020-06-30 06:21:43 +0000

    Input Devise App

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,14 +1,12 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', 
+  
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net', 
     port: '587',
-    domain: 'gmail.com',
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
   
